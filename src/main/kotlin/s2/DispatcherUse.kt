245aa6e1a79b8ea21aad62ca7193777834fc3011
koreaksh <rlatkd0727>
2022-06-17 20:37:05 +0900

    add

--- src/main/kotlin/s2/DispatcherUse.kt
+++ src/main/kotlin/s2/DispatcherUse.kt
@@ -1,13 +1,19 @@
 package s2
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 //상황에맞는 Dispatcher 이용
 
-fun main() {
-    CoroutineScope(Dispatchers.Main).launch {
-        val arrayData 
+fun main() = runBlocking() {
+    val arrayData = async(Dispatchers.IO) {
+        arrayOf(1, 3, 5, 4, 2)
     }
+    val sortArray = async(Dispatchers.Default) {
+        val value = arrayData.await()
+        value.sortedBy { it }
+    }
+
+    val sortedArray = sortArray.await()
+    println(sortedArray)
+
 }
