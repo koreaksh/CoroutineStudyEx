6c16daad50ad6d03093edccd75f136f4f391cb40
koreaksh <rlatkd0727>
2022-06-17 20:25:02 +0900

    DispatcherUse

---
+++ src/main/kotlin/s2/DispatcherUse.kt
@@ -0,0 +1,13 @@
+package s2
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+//상황에맞는 Dispatcher 이용
+
+fun main() {
+    CoroutineScope(Dispatchers.Main).launch {
+        val arrayData 
+    }
+}
